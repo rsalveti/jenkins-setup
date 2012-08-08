29f7e9fed40f2f2a66ff32d24342185ed8799c4c
Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>
2012-08-08 03:12:44 -0300

    init.sh: using sudo for apt-get
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@linaro.org>

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 # install required packages
 
-apt-get install -y --force-yes sed wget cvs subversion git bzr coreutils unzip bzip2 tar gzip cpio gawk python patch diffstat make build-essential gcc g++ desktop-file-utils chrpath autoconf automake libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev texi2html texinfo realpath
+sudo apt-get install -y --force-yes sed wget cvs subversion git bzr coreutils unzip bzip2 tar gzip cpio gawk python patch diffstat make build-essential gcc g++ desktop-file-utils chrpath autoconf automake libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev texi2html texinfo realpath
 
 # clone repositories
 
